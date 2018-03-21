2dd2f68d26429bc5e0230be23aef0fafe0fd5fcf
aleck <aleck.fang@ximalaya.com>
2018-03-22 00:11:13 +0800

    add pod install

--- EnvModifyRelease.sh
+++ EnvModifyRelease.sh
@@ -25,7 +25,7 @@ echo "=================clean================="
 xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}"  -configuration 'Release' clean
 
 echo "+++++++++++++++++build+++++++++++++++++"
-pod install
+/usr/local/bin/pod installpod install
 xcodebuild -workspace "${APP_NAME}.xcworkspace" -scheme "${APP_NAME}" -sdk iphoneos -configuration 'Release'  SYMROOT='$(PWD)' -allowProvisioningUpdates
 
 #构建
